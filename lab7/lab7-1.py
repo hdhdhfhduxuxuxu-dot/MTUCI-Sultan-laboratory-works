8859e04b67c54a6dc3a0ef7c1a11d0e648351351
Sultan <hdhdhfhduxuxuxu@gmail.com>
2026-01-13 23:51:06 +0300

    Update lab7-1.py

--- lab7/lab7-1.py
+++ lab7/lab7-1.py
@@ -2,54 +2,54 @@ class Employee:
     def __init__(self, name, id):
         self.name = name
         self.id = id
-
+    
     def get_info(self):
-        return f"{self.name}, id: {self.id}"
-
-
+        return f"Имя - {self.name}, id - {self.id}"
+    
 class Manager(Employee):
     def __init__(self, name, id, department):
         Employee.__init__(self, name, id)
         self.department = department
-
+    
     def manage_project(self):
-        return f"{self.name} курирует работу отдела {self.department}"
-
+        return f"{self.name} руководит проектом в департаменте {self.department}"
 
 class Technician(Employee):
     def __init__(self, name, id, specialization):
         Employee.__init__(self, name, id)
         self.specialization = specialization
-
+    
     def perform_maintenance(self):
-        return f"{self.name} выполняет задачи по профилю: {self.specialization}"
-
+        return f"{self.name} {self.specialization} выполняет техническое обслуживание"
 
 class TechManager(Manager, Technician):
     def __init__(self, name, id, department, specialization):
         Manager.__init__(self, name, id, department)
         Technician.__init__(self, name, id, specialization)
         self.human = []
+    
+    def manage_project(self):
+        return f"{self.name} руководит проектом в департаменте {self.department}"
 
+    def perform_maintenance(self):
+        return f"{self.name} {self.specialization} выполняет техническое обслуживание"
+    
     def add_employee(self, employee):
         self.human.append(employee)
 
     def get_team_info(self):
-        info = f"Состав команды {self.name}:\n"
+        info = f"Команда {self.name}:\n"
         if not self.human:
-            info += "сотрудников нет"
+            info += "Нет сотрудников"
         else:
             for emp in self.human:
-                info += emp.get_info() + "\n"
+                info += f"{emp.get_info()}\n"
         return info
 
-
-# Демонстрация работы системы
-
-employee1 = Employee("Данил Корнеев", 1)
-manager1 = Manager("Артём Лебедев", 2, "Поддержка")
-technician1 = Technician("Влад Миронов", 3, "DevOps инженер")
-techmanager1 = TechManager("Сергей Фомин", 4, "IT-отдел", "Системный администратор")
+employee1 = Employee("Иван Петров", "1")
+manager1 = Manager("Мария Сидорова", "2", "Разработка")
+technician1 = Technician("Алексей Козлов", "3", "IT")
+techmanager1 = TechManager("Дмитрий Волков", "4", "Сети", "Разработка")
 
 print(employee1.get_info())
 print(manager1.manage_project())
@@ -62,8 +62,4 @@ print(techmanager1.perform_maintenance())
 techmanager1.add_employee(employee1)
 techmanager1.add_employee(manager1)
 techmanager1.add_employee(technician1)
-
 print(techmanager1.get_team_info())
-
-
-
